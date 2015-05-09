c58f714a7571074e9f097fd0d7bec899a2269fa5
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2015-05-08 18:07:29 -0600

    Renamed one of the routing methods

--- lib/tiny_fc.rb
+++ lib/tiny_fc.rb
@@ -2,7 +2,7 @@ module TinyFC
 end
 
 require_relative 'tiny_fc/handler_cache'
-require_relative 'tiny_fc/route_to_handler_in_namespace'
+require_relative 'tiny_fc/route_to_handler_in_namespace_by_http_method'
 require_relative 'tiny_fc/route_to_template_viewing'
 require_relative 'tiny_fc/root_handler'
 require_relative 'tiny_fc/request_handlers'
